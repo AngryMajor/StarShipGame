90c86c2fe737abfa850dc23c9eabee24f68338af
Peter Major <peterfmajor@gmail.com>
2022-10-12 09:20:23 +0100

    added displaying ship region name in card

--- ShipGUICard.gd
+++ ShipGUICard.gd
@@ -7,13 +7,17 @@ func init(ship:Ship):
 	my_ship.connect("DataUpdated",self,"_on_ship_updated")
 	$ShipLabel.text = my_ship.name
 	$APLable.text = String(my_ship.currAP)
-	set_button_color()
+	display_ship_Region()
 	check_ship_available()
 
 func _on_ship_updated():
 	$APLable.text = String(my_ship.currAP)
 	check_ship_available()
+	display_ship_Region()
+
 	
+func display_ship_Region():
+	$LocationLable.text = my_ship.region.name
 	set_button_color()
 	
 func check_ship_available():
